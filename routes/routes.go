1145fedfe0bfb2d1acbbf4123b6a18fa74a7ae3e
atlas <atlas.wbridges@gmail.com>
2024-11-28 18:50:34 +0800

    fixed: websocket finally sending message on the client.

--- routes/routes.go
+++ routes/routes.go
@@ -9,46 +9,49 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
 )
 
-type ErrJSON struct {
-	Message string `json:"message"`
-}
+var (
+	upgrader = websocket.Upgrader{}
+)
 
-func joinChat(ctx *gin.Context) {
-	websocket.Handler(func(ws *websocket.Conn) {
-		defer ws.Close()
+func joinChat(c *gin.Context) {
+	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+	defer ws.Close()
 
-		component := components.Message("Hello Client!")
-		buffer := &bytes.Buffer{}
-		component.Render(context.Background(), buffer)
+	component := components.Message("Hello Client!")
+	buffer := &bytes.Buffer{}
+	component.Render(c, buffer)
+	for {
+		err := ws.WriteMessage(websocket.TextMessage, buffer.Bytes())
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			return
 
-		for {
-			// Write
-			err := websocket.Message.Send(ws, buffer.Bytes())
-			if err != nil {
-				fmt.Printf("Error sending message: %s\n", err)
-				return
-			}
-
-			time.Sleep(time.Second * 10)
-			// Read
-			// msg := ""
-			// err = websocket.Message.Receive(ws, &msg)
-			// if err != nil {
-			// 	log.Fatal(err)
-			// }
-			// fmt.Printf("%s\n", msg)
 		}
-	}).ServeHTTP(ctx.Writer, ctx.Request)
+
+		time.Sleep(time.Second * 1)
+
+		// Read
+		// _, msg, err := ws.ReadMessage()
+		// if err != nil {
+		// 	c.AbortWithError(http.StatusInternalServerError, err)
+		// 	return
+		// }
+		// fmt.Printf("%s\n", msg)
+	}
+
 }
 
-func CreateRoutes(s *gin.Engine) {
-	s.GET("/", func(ctx *gin.Context) {
+func CreateRoutes(c *gin.Engine) {
+	c.GET("/", func(ctx *gin.Context) {
 		component := templates.Index()
 		component.Render(context.Background(), ctx.Writer)
 	})
 
-	s.GET("/ws/chat", joinChat)
+	c.GET("/ws/chat", joinChat)
 }
